9b26f47a13a105f97853d41e4daddb0ca05b21d5
Philipp Sperber <philipp.sperber+github@gmail.com>
2017-12-18 18:52:42 +0100

    initial commit

---
+++ IOTA.lua
@@ -0,0 +1,113 @@
+-- Inofficial IOTA Extension for MoneyMoney
+-- Fetches IOTA quantity for addresses via IOTASear.ch
+-- Fetches IOTA price in EUR via coinmarketcap API
+-- Returns cryptoassets as securities
+--
+-- Username: IOTA Adresses comma seperated
+-- Password: [Whatever]
+
+-- MIT License
+
+-- Copyright (c) 2017 PSperber
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+
+WebBanking {
+    version = 0.1,
+    description = "Include your IOTAs as cryptoportfolio in MoneyMoney by providing IOTA addresses as usernme (comma seperated) and a random Password",
+    services = { "IOTA" }
+}
+
+local iotaAddress
+local connection = Connection()
+local currency = "EUR" -- fixme: make dynamik if MM enables input field
+
+function SupportsBank(protocol, bankCode)
+    return protocol == ProtocolWebBanking and bankCode == "IOTA"
+end
+
+function InitializeSession(protocol, bankCode, username, username2, password, username3)
+    iotaAddress = username:gsub("%s+", "")
+end
+
+function ListAccounts(knownAccounts)
+    local account = {
+        name = "IOTA",
+        accountNumber = "IOTA",
+        currency = currency,
+        portfolio = true,
+        type = "AccountTypePortfolio"
+    }
+
+    return { account }
+end
+
+function RefreshAccount(account, since)
+    local s = {}
+    prices = requestIOTAPrice()
+
+    for address in string.gmatch(iotaAddress, '([^,]+)') do
+        iotaQuantity = requestIOTAQuantityForIOTAAddress(address)
+
+        s[#s + 1] = {
+            name = address,
+            currency = nil,
+            market = "cryptocompare",
+            quantity = iotaQuantity,
+            price = prices["price_eur"],
+        }
+    end
+
+    return { securities = s }
+end
+
+function EndSession()
+end
+
+
+-- Querry Functions
+function requestIOTAPrice()
+    response = connection:request("GET", cryptocompareRequestUrl(), {})
+    json = JSON(response)
+
+    return json:dictionary()[1]
+end
+
+--
+function requestIOTAQuantityForIOTAAddress(iotaAddress)
+    response = connection:request("GET", iotaRequestUrl(iotaAddress), {})
+    html = HTML(response)
+    elements = html:xpath("//div[@class='summary-info']/div[3]/span[2]")
+    value = elements:get(1):text()
+    for s in string.gmatch(value, '%S+') do
+        return s
+    end
+end
+
+
+-- Helper Functions
+function cryptocompareRequestUrl()
+    return "https://api.coinmarketcap.com/v1/ticker/iota/?convert=EUR"
+end
+
+function iotaRequestUrl(iotaAddress)
+    return "https://iotasear.ch/address/" .. iotaAddress
+end
+
--- README.md
+++ README.md
@@ -1,26 +1,28 @@
-# Bitcoin-MoneyMoney
-Fetches amount and value of privately held Bitcoin address via blockexplorer and returns it as a security.  
+# IOTA-MoneyMoney
+Fetches amount and value of privately held IOTA address via IOTASear.ch and returns it as a security.
 This can be also be used to track your cold storage.
 
-![MoneyMoney screenshot with Bitcoin Balance](screens/bitcoin-balance.png)
+Do you like this extension? Donation IOTA address: OUDFKXFDRSWBCTRSJ9MMFGVENOFGHCADQTJUIKTZJPLPGHEQBCLCZPIANOZ9NO9XQVRRHJNOIJNJTPJRWJBWJXHGFW
+
+This repository is heavily inspired by [Bitcoin-MoneyMoney](https://github.com/Jacubeit/Bitcoin-MoneyMoney). Thanks!
 
 ## Extension Setup
 
 You can get a signed version of this extension from
 
-* my [GitHub releases page](https://github.com/Jacubeit/Bitcoin-MoneyMoney/releases/tag/v0.1), or
+* my [GitHub releases page](https://github.com/psperber/IOTA-MoneyMoney/releases/tag/v0.1), or
 * the [MoneyMoney Extensions](https://moneymoney-app.com/extensions/) page
 
-Once downloaded, move `Bitcoin.lua` to your MoneyMoney Extensions folder.
+Once downloaded, move `IOTA.lua` to your MoneyMoney Extensions folder.
 
 **Note:** This extension requires MoneyMoney **Version 2.2.19** or newer.
 
 ## MoneyMoney Setup
 
-Add a new account (type "Bitcoin"). 
+Add a new account (type "IOTA"). 
 
-**Use your Bitcoin adresses coma seperated as user name**  
-`1KuWLoZuoJgz3N6sLoAwGth9XGm8YuFTGt, 1KuWLoZuoJgz3N6sLoAwGth9XGm8YuFTGt` (example)
+**Use your IOTA adresses coma seperated as user name**  
+`1KuWLoZuoJgz3N6sLoPwGth9XGm8YuFTGt, 1KuWLoZuoJgz3N6sLoPwGth9XGm8YuFTGt` (example)
 
 **Use whatever you want as password**  
 `123` (example)
